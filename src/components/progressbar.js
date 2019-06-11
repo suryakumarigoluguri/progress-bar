6d2c1709af3d768ffa8b4d14b092017807f4f0b5
Surya Kumari Goluguri <49465013+suryakumarigoluguri@users.noreply.github.com>
2019-06-11 16:03:04 +0530

    Update progressbar.js

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -2,7 +2,6 @@
 //Reference link https://www.npmjs.com/package/react-step-progress-bar
 
 import React from "react";
-import "react-step-progress-bar/styles.css";
 import './progressbar.css'
 import { ProgressBar, Step } from "react-step-progress-bar";
 
